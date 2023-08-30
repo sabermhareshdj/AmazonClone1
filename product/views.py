2cd6f824785b5336bed872daa2bcab57fc1a9996
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-08-30 13:58:22 +0300

    add brand list

--- product/views.py
+++ product/views.py
@@ -17,3 +17,7 @@ class ProductDetail(DetailView):
         context["reviews"] = Review.objects.filter(product=self.get_object())
         context["related_products"] = Product.objects.filter(brand=self.get_object().brand)
         return context
+    
+
+class BrandList(ListView):
+    model = Brand
