151fa495b086fd37792ae37908ad130d33b514b1
dj.saber.mharesh <dj.saber.mharesh@gmail.com>
2023-08-31 12:17:55 +0300

    add image

--- dummy_data.py
+++ dummy_data.py
@@ -12,15 +12,31 @@ def seed_brand(n):
     for _ in range(n):
         Brand.objects.create (
             name = fake.name(),
-            image = f'brands/{images[random.randint(0,11)]}'
+            image = f'brands/{images[random.randint(0,10)]}'
         )
     print (f'seed {n} Brands Successfully')
 
 
 def seed_product(n):
-    pass
+    fake = Faker()
+    images = ['2.png','3.png','4.png','5.png','6.png','7.png','8.png','9.png','10.jpg','11.png','12.jpg','13.jpg']
+    flags = ['New','Sale','Feature']
+
+    for _ in range(n):
+        Product.objects.create(
+            name = fake.name(),
+            image = f'brands/{images[random.randint(0,10)]}' ,
+            flag = flags[random.randint(0,2)] ,
+            price = round(random.uniform(20.99,99.99),2 ),
+            sku = random.randint(1000,10000000) ,
+            subtitle = fake.text(max_nb_chars=250) ,
+            description = fake.text(max_nb_chars=2000) ,
+            quantity = random.randint(0,30) ,
+            brand = Brand.objects.get(id=random.randint(1,105))
+        )
+
+        print (f'seed {n} Product Successfully')
 
 
-
-
-seed_brand(5)
+#seed_brand(1000)
+seed_product(2000)
